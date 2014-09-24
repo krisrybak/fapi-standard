989ac1839672d282475ba3aa8e0e76a35427b35c
krisrybak <kris.rybak@hotmail.co.uk>
2014-09-24 08:22:37 +0100

    Generates basic folder structure for application

--- web/app.php
+++ web/app.php
@@ -1,16 +1,7 @@
 <?php
+
 /**
- * Fapi bootstarp file
+ * Fapi-standard bootstarp file
  * @author  Kris Rybak <kris@krisrybak.com>
- * @package fapi
+ * @package fapi-standard
  */
-
-use Symfony\Component\HttpFoundation\Request;
-
-require_once __DIR__.'/../app/AppKernel.php';
-
-$app = new AppKernel('prod', false);
-
-$request    = Request::createFromGlobals();
-$response   = $app->handle($request);
-$response->send();
